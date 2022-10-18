5edd72039d2f3a6adc996bf0a49048cc8503569c
Amardeep Mann <amar2slick@gmail.com>
2022-10-18 11:32:04 -0700

    Moved ToLower() to conditional

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿// Amardeep Mann
-// 09-13-2022
-// Challenge #0 - MC#1 "What is your name?"
+// 10-18-22
+// Mini Challenge #1 - "What is your name?"
 // Project that will ask user their name and output it back to the console
-// Ch#0 Peer Review by: Lerissa Lazar, I like how enthusiastic you sound in all your outputs, everything worked well and smoothly. Great job!
 
 Console.Clear();
 
@@ -13,11 +12,11 @@ bool playAgain = true;
 while (playAgain) {
   Console.WriteLine("What is your name? (Type \"exit\" to quit)");
 
-  string userName = Console.ReadLine().ToLower();
+  string userName = Console.ReadLine();
 
-  if (userName == "exit") {
-    playAgain =false;
+  if (userName.ToLower() == "exit") {
+    playAgain = false;
   } else {
-    Console.WriteLine("\nTop of the morning, " + userName + ". Have a corntastic day!\n");
+    Console.WriteLine("\nGood day to you, " + userName + ". Blessed be the fruit!\n");
   }
 }
